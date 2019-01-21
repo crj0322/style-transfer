64d990941408862fb07ec9dc02d1b93588dfb91b
crj0322 <crj_0322@qq.com>
2019-01-21 15:41:00 +0800

    add center crop function

--- utils.py
+++ utils.py
@@ -13,3 +13,11 @@ def squar_resize(img, dst_size):
         img = img[:, col_start:col_end, :]
 
     return cv.resize(img, (dst_size, dst_size))
+
+def center_crop(img, dst_size, row_offset=0, col_offset=0):
+    rows, cols = img.shape[:2]
+    assert rows > dst_size[0] and cols > dst_size[1]
+    x, y = (rows + row_offset)//2, (cols + col_offset)//2
+    left = y - dst_size[1]//2
+    up = x - dst_size[0]//2
+    return img[up:up+dst_size[0], left:left+dst_size[1], :]
